8a077708e222ca468337d42272fc814959204998
Ahmad Almasri <ahmadalmasri@Ahmads-MacBook-Pro.local>
2019-09-07 14:52:29 +0300

    add scroll position

--- Example/WinSlidShow/ViewController.swift
+++ Example/WinSlidShow/ViewController.swift
@@ -21,26 +21,29 @@ class ViewController: UIViewController {
 	}
 	
 	private func setupRemoteSloder() {
-		for i in 0...5 {
-		remoteSlider.setImage(source: .remote([ "https://picjumbo.com/wp-content/uploads/pienza-town-in-tuscany_free_stock_photos_picjumbo_DSC04564-2210x1473.jpg",
-			"https://i0.wp.com/picjumbo.com/wp-content/uploads/tuscany-sunset.jpg",
-			"https://picjumbo.com/wp-content/uploads/traveling-to-manarola-cinque-terre-follow-me-to-free-photo-DSC04349-2210x1473.jpg",
-			"https://i0.wp.com/picjumbo.com/wp-content/uploads/giau-pass-mountain-la-gusela-peak-dolomites-italy.jpg",
-			"https://i2.wp.com/picjumbo.com/wp-content/uploads/sea-coastline-italy-free-photo-DSC04430.jpg",
-			"https://i2.wp.com/picjumbo.com/wp-content/uploads/yummy-fresh-burger-with-french-fries_free_stock_photos_picjumbo_DSC03833.jpg",
-			"https://i1.wp.com/picjumbo.com/wp-content/uploads/tasty-colorful-muffins_free_stock_photos_picjumbo_IMG_3511.jpg",
-			"https://i0.wp.com/picjumbo.com/wp-content/uploads/20180825-DSC05894.jpg",
-			"https://i0.wp.com/picjumbo.com/wp-content/uploads/20180825-DSC05894.jpg",
-			"https://i2.wp.com/picjumbo.com/wp-content/uploads/bali-temple-of-lempuyang.jpg",
-			"https://i1.wp.com/picjumbo.com/wp-content/uploads/young-happy-woman-enjoying-freedom-on-a-walk-free-photo-DSC01183.jpg"
-			], placeholder: #imageLiteral(resourceName: "placeholder")))
+		 var images = [String]()
+		for i in 0...20 {
+		images.append(contentsOf: 	[ "https://picjumbo.com/wp-content/uploads/pienza-town-in-tuscany_free_stock_photos_picjumbo_DSC04564-2210x1473.jpg",
+			  "https://i0.wp.com/picjumbo.com/wp-content/uploads/tuscany-sunset.jpg",
+			  "https://picjumbo.com/wp-content/uploads/traveling-to-manarola-cinque-terre-follow-me-to-free-photo-DSC04349-2210x1473.jpg",
+			  "https://i0.wp.com/picjumbo.com/wp-content/uploads/giau-pass-mountain-la-gusela-peak-dolomites-italy.jpg",
+			  "https://i2.wp.com/picjumbo.com/wp-content/uploads/sea-coastline-italy-free-photo-DSC04430.jpg",
+			  "https://i2.wp.com/picjumbo.com/wp-content/uploads/yummy-fresh-burger-with-french-fries_free_stock_photos_picjumbo_DSC03833.jpg",
+			  "https://i1.wp.com/picjumbo.com/wp-content/uploads/tasty-colorful-muffins_free_stock_photos_picjumbo_IMG_3511.jpg",
+			  "https://i0.wp.com/picjumbo.com/wp-content/uploads/20180825-DSC05894.jpg",
+			  "https://i0.wp.com/picjumbo.com/wp-content/uploads/20180825-DSC05894.jpg",
+			  "https://i2.wp.com/picjumbo.com/wp-content/uploads/bali-temple-of-lempuyang.jpg",
+			  "https://i1.wp.com/picjumbo.com/wp-content/uploads/young-happy-woman-enjoying-freedom-on-a-walk-free-photo-DSC01183.jpg"
+			])
+		}
+		remoteSlider.setImage(source: .remote(images, placeholder: #imageLiteral(resourceName: "placeholder")))
 		remoteSlider.pageControlconfig.borderWidth = 1
 		remoteSlider.pageControlconfig.borderColor = .orange
 		remoteSlider.pageControlconfig.currentPageTintColor = .orange
 		remoteSlider.pageControlconfig.inactiveTintColor = .white
 		remoteSlider.pageControlconfig.radius = 7
 		remoteSlider.showPageControl = true
-		}
+		
 	}
 	private func setupLocalSloder() {
 		localSlider.setImage(source: .local([#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local"),#imageLiteral(resourceName: "local")]))
